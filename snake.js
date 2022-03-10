728305043c807de0326ce4be0017d85fa550861c
YATHAM <yatham024@gmail.com>
2022-03-10 23:14:46 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -29,3 +29,25 @@ if( !window.cancelAnimationFrame ) {
 }
 
 })();
+
+(function(){ 'use strict';
+
+function hasClass( elem, className ) {
+  return new RegExp( ' ' + className + ' ' ).test( ' ' + elem.className + ' ' );
+};
+
+function addClass( elem, className ) {
+  if( !hasClass(elem, className ) ) {
+    elem.className += ' ' + className;
+  }
+};
+
+function removeClass( elem, className ) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
+  if( hasClass( elem, className ) ) {
+    while( newClass.indexOf(' ' + className + ' ' ) >= 0 ) {
+      newClass = newClass.replace( ' ' + className + ' ', ' ' );
+    }
+    elem.className = newClass.replace( /^\s+|\s+$/g, '' );
+  }
+};
