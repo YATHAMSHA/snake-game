5d97ab4b8078b89e8edec5d91f75741190d45b31
YATHAM <yatham024@gmail.com>
2022-03-17 23:57:55 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -197,6 +197,29 @@ g.Time.prototype.update = function() {
 
 })();
 
+(function(){ 'use strict';
+
+g.Grid = function( cols, rows ) {
+  this.cols = cols;
+  this.rows = rows;
+  this.tiles = [];
+  for( var x = 0; x < cols; x++ ) {
+    this.tiles[ x ] = [];
+    for( var y = 0; y < rows; y++ ) {
+      this.tiles[ x ].push( 'empty' );
+    }
+  }
+};
+
+g.Grid.prototype.get = function( x, y ) {
+  return this.tiles[ x ][ y ];
+};
+
+g.Grid.prototype.set = function( x, y, val ) {
+  this.tiles[ x ][ y ] = val;
+};
+
+})();
 
 
 
